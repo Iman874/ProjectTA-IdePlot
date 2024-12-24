7c000d5ccc5881c1187f1a5de0ab88af864430b5
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2024-12-24 03:24:49 +0000

    update

--- app/src/main/java/com/rosuliman/projectta_ideplot/HalamanDaftar.kt
+++ app/src/main/java/com/rosuliman/projectta_ideplot/HalamanDaftar.kt
@@ -1,8 +1,11 @@
 package com.rosuliman.projectta_ideplot
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
@@ -30,16 +33,19 @@ class HalamanDaftar : AppCompatActivity() {
         val email = binding.formEmailDaftar.toString()
         val password = binding.formPassword1Daftar.toString()
 
-
-
         // Set content view dengan root dari binding
         setContentView(binding.root)
 
         // Panggil fungsi untuk mengatur listener pada loginText
         setupLoginText()
 
-        binding.btnDaftar.setOnClickListener(){
-            registerUser(email, nama, password)
+        binding.btnDaftar.setOnClickListener{
+            if (isInternetAvailable()) {
+                registerUser(email, nama, password)
+            } else {
+                // Menampilkan pesan Toast jika tidak ada koneksi internet
+                Toast.makeText(this, "Tidak dapat terhubung ke jaringan internet", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
@@ -75,8 +81,15 @@ class HalamanDaftar : AppCompatActivity() {
         startActivity(intent)
     }
 
+    // Fungsi untuk mengecek koneksi internet
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnected
+    }
+
     private fun registerUser(email: String, name: String, password: String) {
-        val url = "http://192.168.68.71:3000/register" // Ganti dengan IP server jika perlu
+        val url = "https://automatic-umbrella-7v649xxx446cw459-3000.app.github.dev/register" // Ganti dengan URL server yang benar
 
         val jsonObject = JSONObject()
         try {
@@ -101,7 +114,6 @@ class HalamanDaftar : AppCompatActivity() {
 
                 // Log error secara rinci untuk membantu debugging
                 Log.e("HalamanDaftar", "Registration failed: ${error.message}", error)
-
             })
 
         // Menambahkan request ke queue
